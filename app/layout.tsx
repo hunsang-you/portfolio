037f6abb90aaa81ed97e9badfe510a225d96a6ba
hunsang <gjstkd123@naver.com>
2026-05-08 01:28:37 +0900

    layout-프론트엔트 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,8 +8,8 @@ import { Toaster } from "@/components/ui/toaster";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "프론트엔드 주니어 개발자, 유헌상의 Portfolio",
-  description: "프론트엔드 주니어 개발자 포트폴리오",
+  title: "주니어 개발자, 유헌상의 Portfolio",
+  description: "주니어 개발자 포트폴리오",
   generator: "v0.dev",
 };
 
